ddd22fbce5904983e4981810c2da330bd28b5a1f
Ethan <ethansagin@gmail.com>
2019-02-08 20:11:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -127,7 +127,9 @@ function endGame() {
 
 function moveDodger(e) {
     if(e.which === LEFT_ARROW) {
-      moveDodgerLeft()
+      e.preventDefault();
+      e.stopPropagation();
+      moveDodgerLeft();
     } else if(e.which === RIGHT_ARROW) {
       moveDodgerRight()
     }
