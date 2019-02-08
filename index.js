cc783a04f2d55031acf8d76366f9eb106d46c52a
Ethan <ethansagin@gmail.com>
2019-02-08 20:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ GAME.appendChild('rock')
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
+   rock.style.top = `${top += 2}px`
 if(checkCollision(rock)) {
   endGame();
 } else if(top < GAME_HEIGHT - 20) {
